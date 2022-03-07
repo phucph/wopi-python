2866b8c2a36481a0f11eeaa78817393c6aa60533
phucph <phucph@gemvietnam.com>
2022-03-07 17:45:33 +0700

    up code

---
+++ wopiserver/urls.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2018-2-10
+
+@author: qi
+'''
+from django.urls import path, re_path
+from . import views
+
+app_name = 'wopiserver'
+urlpatterns = [
+    re_path(r'files/(?P<fileid>[^/]+)$', views.wopiGetFileInfo),
+#     url(r'^files/(?P<fileid>[^/]+)/$',views.wopiGetFileInfo),
+    re_path(r'files/(?P<fileid>[^/]+)/contents$', views.wopiFileContents),
+#     url(r'^files/(?P<fileid>[^/]+)/contents/$',views.wopiFileContents),
+]
+
